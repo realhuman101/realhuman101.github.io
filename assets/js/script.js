008f84a4976412a9b9cefe8990d6b61692b1d7c4
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-08-05 00:23:07 +0200

    added hamburger menu
    for small screen users

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,42 @@
 $(window).on('load',() => {
 	$('#loader').fadeOut('fast');
+
+	$('#nav-hamburger').click(() => {
+		$('#hamburger-inner').toggleClass('hamburger-clicked');
+		$('#hamburger-items').toggleClass('hamburger-showing');
+
+		if ($('#hamburger-items').css('display') == 'none') {
+			$('#hamburger-items').css({'display': 'flex'})
+			$('#hamburger-inner').addClass('hamburger-color');
+		} else {
+			$('#hamburger-items').fadeOut(300).delay(400);
+			$('#hamburger-items').css({'display': 'none'})
+			$('#hamburger-inner').removeClass('hamburger-color');
+			checkNavScroll();
+		}
+	})
 })
 
 $(window).on('scroll', () => {
+	checkNavScroll()
+})
+
+function checkNavScroll() {
 	const navbar = $('#navbar');
+	const hamburger = $('#nav-hamburger')
 	const navY = $('#navY');
 
 	if ($(window).scrollTop() >= navY.offset().top) {
-		navbar.css({"position": "fixed","top": "0px"});
-		navY.css({"height": '60px'});
+		if ($('#hamburger-items').css('display') == 'none') {
+			$('#hamburger-inner').removeClass('hamburger-color');
+			navbar.css({"position": "fixed","top": "0px"});
+			navY.css({"height": '60px'});
+		}
 	} else {
-		navbar.css({"position": "relative","top": "0px"});
-		navY.css({"height": "0px"});
+		if ($('#hamburger-items').css('display') == 'none') {
+			$('#hamburger-inner').addClass('hamburger-color');
+			navbar.css({"position": "relative","top": "0px"});
+			navY.css({"height": "0px"});
+		}
 	}
-})
+}
