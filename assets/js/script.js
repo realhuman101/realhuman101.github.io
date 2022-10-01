cc98c7c907c8272b3b5cd5e4227e723a98fd8768
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-10-01 13:45:59 +0800

    added lazy loading text

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,6 +151,22 @@ $(window).on('load',() => {
 	$(window).on('scroll', () => {
 		checkNavScroll();
 	})
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('visible');
+			} else {
+				entry.target.classList.remove('visible');
+			}
+		});
+	});
+
+	const hiddenElements = document.querySelectorAll('.hidden');
+	hiddenElements.forEach(element => {
+			observer.observe(element);
+		}
+	);
 })
 
 function checkNavScroll() {
