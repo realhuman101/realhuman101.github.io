f1ce15586f419d4fa9bf309d5e2d373ed155f825
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-06-25 18:25:02 +0800

    add orbit controls and GLTF loader

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
