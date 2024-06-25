ba9d5c336a787351f4344b980d3b94acb526f91f
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-06-25 19:36:49 +0800

    Add GLTF

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -16,28 +15,27 @@ const light = new THREE.PointLight(0xffffff, 1000)
 light.position.set(10, 130, 15)
 scene.add(light)
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
-const mtlloader = new MTLLoader();
+const loader = new GLTFLoader();
 
-// Load desk
-mtlloader.load('assets/models/Desk.mtl', 
-	(materials) => {
-		materials.preload()
-		const OBJloader = new OBJLoader();
-		OBJloader.setMaterials(materials)
+// Load in desk
+loader.load('assets/models/Desk.gltf', (gltf) => {
+		scene.add(gltf.scene);
+	},
+	// called while loading is progressing
+	function ( xhr ) {
 
-		OBJloader.load('assets/models/Desk.obj', (object) => {
-			scene.add(object)
-		},
-		(xhr) => {
-			console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-		},
-		(error) => {
-			console.log('An error happened', error)
-		})
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened', error );
+
 	}
 )
 
@@ -50,7 +48,7 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+    // render()
 }
 
 function animate() {
