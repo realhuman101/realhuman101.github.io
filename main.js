009c2c06eae15f5ead491c0d07c9341bde1f0bcb
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-06-27 18:50:53 +0800

    remove unnecessary code

--- main.js
+++ main.js
@@ -586,7 +586,6 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    // render()
 }
 
 // -----------
