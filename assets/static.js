e0eeb76a41250238b35d0647f832d2f15532c899
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-06-27 19:11:59 +0800

    Make loading screen slide out

---
+++ assets/static.js
@@ -0,0 +1,14 @@
+function sleep(time) {
+	return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function completeLoad() {
+	let loading = document.getElementById('loading')
+	
+	loading.style.animationFillMode = 'forwards'
+	loading.style.animation = '250ms linear slideUp'
+	
+	sleep(250).then(() => {
+		loading.style.display = 'none'
+	})
+}
